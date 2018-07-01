5a41564b93317488b4a40f3be8de057072d288a3
chuangmaster <chuang.master@gmail.com>
2018-07-01 21:21:02 +0800

    加入Common.Base 專案、加入BotEventTypeEnums

---
+++ Bot.Common.Base/Enums/BotEventTypeEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Bot.Common.Base.Enums
+{
+    /// <summary>
+    /// enum BotEventTypeEnum
+    /// </summary>
+    public enum BotEventTypeEnum
+    {
+        [Description("text")]
+        Text = 1,
+        [Description("image")]
+        Image = 2,
+        [Description("video")]
+        Video = 3,
+        [Description("audio")]
+        Audio = 4,
+        [Description("location")]
+        Location = 5
+    }
+}
